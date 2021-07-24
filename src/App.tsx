cf03ae1233e275797be8955a6a8c3f6456c0fc29
Vinicius Serafim <dev.gestao.front@saurus.com.br>
2021-07-24 16:34:12 -0300

    deploy rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import api from './services/api'
+import { useEffect } from 'react';
+import { Rotas } from './components';
+
 function App() {
+  const [senhaMd5, setSenhaMd5] = useState('');
+  useEffect(()=>{
+    api.get("login/1234").then(({data}) =>{
+      setSenhaMd5(data.senha)
+    })
+  },[])
   return (
     <>
-      Hello world!
+      <Rotas></Rotas>
     </>
   );
 }
---
+++ src/components/rotas/Rotas.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { CadastroUsuario } from "../../paginas";
+
+export function Rotas() {
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path="/usuario" component={CadastroUsuario}></Route>
+                </Switch>
+            </Router>
+        </>
+    )
+}
