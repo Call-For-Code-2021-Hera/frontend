11b535ebdbbb1387401daeb5d91246ecc6b772e2
Vinicius Serafim <dev.gestao.front@saurus.com.br>
2021-07-24 16:41:36 -0300

    deploy rotas

--- src/components/rotas/Rotas.tsx
+++ src/components/rotas/Rotas.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { CadastroUsuario } from "../../paginas";
 
 export function Rotas() {
